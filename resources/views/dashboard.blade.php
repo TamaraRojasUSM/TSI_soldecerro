d47efdac5cccc0a2e86ec5079c996a81b4ac3d4a
Mauricio Rojas <mrojasb2000@gmail.com>
2025-11-02 22:04:26 -0300

    admin profile validation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,20 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+
+                @auth
+                    
+                    @if(Auth::user()->profile->name == 'Administrador')
+                        <div class="p-6 text-gray-900">
+                            {{ __("You're logged in as Administrator!") }}
+                        </div>
+                    @else
+                        <div class="p-6 text-gray-900">
+                            {{ __("You're logged in!") }}
+                        </div>
+                    @endif
+                @endauth
+                
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,6 +28,10 @@
                             href="{{ url('/dashboard') }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                         >
+                        {{ Auth::user()->profile->name }}
+                            {{-- @if(Auth::user()->profile().name == 'Administrador')
+                                    Dashboard
+                            @endif --}}
                             Dashboard
                         </a>
                     @else
