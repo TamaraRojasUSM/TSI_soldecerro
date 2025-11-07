c9fcab7a0cbac3fc3f9216ff457005f685e2ff68
Mauricio Rojas <mrojasb2000@gmail.com>
2025-11-07 16:52:38 -0300

    refactor maintenance model

--- app/Models/Maintenance.php
+++ app/Models/Maintenance.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Maintenance extends Model
 {
-    //
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'instrument_id',
+        'maintenance_at',
+        'observaciones',
+    ];
+
+    public function instrument()
+    {
+        return $this->belongsTo(Instrument::class);
+    }
 }
